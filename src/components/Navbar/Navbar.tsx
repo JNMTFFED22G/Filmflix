768f38204e20b66ac10d3f917eafb746e93d2e1e
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-12-05 23:56:38 +0100

    run format

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -9,21 +9,39 @@ export default function Navbar() {
   const [isSearchModalOpen, setIsSearchModalOpen] = useState(false);
 
   const toggleSearchModal = () => {
-    setIsSearchModalOpen((prev) => !prev);
+    setIsSearchModalOpen(prev => !prev);
   };
 
   return (
-    <nav className="navbar">
-      <h1><Link to="/" className="logo">FILMFLIX</Link></h1>
+    <nav className='navbar'>
+      <h1>
+        <Link to='/' className='logo'>
+          FILMFLIX
+        </Link>
+      </h1>
       <ul>
         <li>
-          <span className="open-close-icons" onClick={toggleSearchModal}>
+          <span className='open-close-icons' onClick={toggleSearchModal}>
             <FontAwesomeIcon icon={isSearchModalOpen ? faTimes : faSearch} />
           </span>
         </li>
-        <li><Link style={{ color: '#000000', textDecoration: 'none' }} to="/categories">Categories</Link></li>
-        <li><Link style={{ color: '#000000', textDecoration: 'none' }} to="/bookmarks">Bookmarks</Link></li>
-        <span className="hamburger-span"></span>
+        <li>
+          <Link
+            style={{ color: '#000000', textDecoration: 'none' }}
+            to='/categories'
+          >
+            Categories
+          </Link>
+        </li>
+        <li>
+          <Link
+            style={{ color: '#000000', textDecoration: 'none' }}
+            to='/bookmarks'
+          >
+            Bookmarks
+          </Link>
+        </li>
+        <span className='hamburger-span'></span>
       </ul>
 
       {isSearchModalOpen && <SearchModal onClose={toggleSearchModal} />}
