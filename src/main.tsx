c1b4ee14d2d44e4cefc8326e1f937bd957149574
Jenny Pettersson <j.m.pettersson@hotmail.se>
2023-12-05 11:09:59 +0100

    added routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,60 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import App from './App.tsx';
+import './index.css';
+import BookMarks from './pages/BookMarks.tsx';
+import Categories from './pages/Categories.tsx';
+import FilmView from './pages/FilmView.tsx';
+import HomePage from './pages/HomePage.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+//TODO:
+// Create a 404 page
+// Create a single category page
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path='/'
+      element={<App />}>
+      <Route
+        index
+        path='/'
+        element={<HomePage />}
+      />
+      <Route
+        path='categories'
+        element={<Categories />}>
+        <Route
+          path=':genre'
+          element={<div>Category</div>}
+        />
+      </Route>
+      <Route
+        path=':slug'
+        element={<FilmView />}
+      />
+      <Route
+        path='/bookmarks'
+        element={<BookMarks />}
+      />
+      <Route
+        path='*'
+        element={<div>404</div>}
+      />
+    </Route>,
+  ),
+);
+
+ReactDOM.createRoot(
+  document.getElementById('root')!,
+).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
