9b65a3f670989336d334d024feba46605fbe667c
Jenny Pettersson <j.m.pettersson@hotmail.se>
2023-12-06 12:25:58 +0100

    rec variant initial styling

--- src/components/Thumbnail/Thumbnail.tsx
+++ src/components/Thumbnail/Thumbnail.tsx
@@ -1,3 +1,5 @@
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import placeholder from '../../img/placeholder.jpg';
 import iMovie from '../../types/iMovie';
 import './Thumbnail.css';
@@ -6,14 +8,36 @@ interface ThumbnailProps {
   movie: iMovie;
 }
 
+const TrendingTitle: React.FC<ThumbnailProps> = ({ movie }) => {
+  return (
+    <div className='titleContainer'>
+      <div className='movieTitle'>{movie.title} -</div>
+      <div className='movieYear'>{movie.year}</div>
+    </div>
+  );
+};
+
+const RecommendedTitle: React.FC<ThumbnailProps> = ({ movie }) => {
+  return (
+    <div className='recTitleContainer'>
+      <div className='firstRow'>
+        <div className='recMovieTitle'>{movie.title}</div>
+        <FontAwesomeIcon icon={faHeart} />
+      </div>
+      <div className='secondRow'>
+        <div className='recMovieYear'>{movie.year}</div>
+        <div>{movie.rating} rating</div>
+      </div>
+    </div>
+  );
+};
+
 const Thumbnail: React.FC<ThumbnailProps> = ({ movie }) => {
   return (
     <>
       <div className='outerDiv'>
-        <div className='titleContainer'>
-          <div className='movieTitle'>{movie.title} -</div>
-          <div className='movieYear'>{movie.year}</div>
-        </div>
+        <RecommendedTitle movie={movie} />
+
         <img
           className='image'
           src={movie.thumbnail ? movie.thumbnail : placeholder}
