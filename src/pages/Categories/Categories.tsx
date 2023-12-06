fe62faf6b060ef1e520c5d9d47bfda98fa0adfbd
felicia <feliciawillnas@gmail.com>
2023-12-06 16:34:14 +0100

    able to render unique categories

--- src/pages/Categories/Categories.tsx
+++ src/pages/Categories/Categories.tsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from 'react';
+import movies from '../../data/movies.json';
+import './Categories.css';
+import '/src/App.css';
+
 export default function Categories() {
+  const [categories, setCategories] = useState<string[]>([]);
+
+  useEffect(() => {
+    const uniqueCategories = [
+      ...new Set(
+        movies.flatMap(movie => movie.genre.split(', ')).filter(genre => genre)
+      ),
+    ];
+    setCategories(uniqueCategories);
+  }, []);
+
   return (
-    <div>
-      <h1>Categories</h1>
+    <div className='categories-wrapper'>
+      {categories.map(category => (
+        <div className='category-container' key={category}>
+          <h2 className='category-item'>{category}</h2>
+        </div>
+      ))}
     </div>
   );
 }
