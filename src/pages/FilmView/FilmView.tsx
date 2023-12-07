5792e6b862a535ba9ba0d1845c41d2b4d0138b1e
NathanaelBlackbourn <nathanael.blackbourn@hoy.se>
2023-12-07 08:46:44 +0100

    Build FimView page

--- src/pages/FilmView/FilmView.tsx
+++ src/pages/FilmView/FilmView.tsx
@@ -1,10 +1,42 @@
-// import { useParams } from "react-router-dom";
-// import films from "../data/movies.json"
+import styles from './FilmView.module.css';
+
+import { useParams } from 'react-router-dom';
+import moviesJSON from '../../data/movies.json';
+import iMovie from '../../types/iMovie';
 
 export default function FilmView() {
-  // const { slug } = useParams<{ slug: string }>();
+  const { slug } = useParams<{ slug: string }>();
 
-  // const { title, year, rating, actors, genre, synopsis, thumbnail } = JSON.parse(films).find((film: any) => film.slug === slug);
+  const movie = moviesJSON.find((film: iMovie) => film.slug === slug);
 
-  return <div className='section' />;
+  if (!movie) {
+    return <div className='section'>404</div>;
+  }
+
+  const { title, year, rating, actors, genre, synopsis, thumbnail } = movie;
+
+  return (
+    <div className={`section ${styles.section}`}>
+      <div className={styles.vertPos}>
+        <div className={`${styles.block} ${styles.imageContainer}`}>
+          <img src={thumbnail} />
+        </div>
+        <div className={`${styles.block} ${styles.textContainer}`}>
+          <div className={styles.textBlock}>
+            <h2>
+              {title}
+              <span className={`${styles.year} ${styles.gapLeft}`}>{year}</span>
+            </h2>
+            <p>{actors.join(', ')}</p>
+            <p className={styles.ratingGenre}>
+              <span>{`Rating ${rating}`}</span>
+              <span className={styles.gapLeft}>{genre}</span>
+            </p>
+            <p className={styles.synopsis}>{synopsis}</p>
+            {/* TODO: Insert bookmark button here*/}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
