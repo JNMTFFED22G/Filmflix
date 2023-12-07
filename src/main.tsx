ef7be8a6a8d06e8c625e8bcffc01bcc42290a095
Jenny Pettersson <j.m.pettersson@hotmail.se>
2023-12-07 16:02:50 +0100

    installed mantine carousel

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
+import '@mantine/carousel/styles.css';
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.layer.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import { createBrowserRouter } from 'react-router-dom';
-import { MantineProvider } from '@mantine/core';
 import routes from './routes';
 
 const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-  <MantineProvider>
-    <RouterProvider router={router} />
-  </MantineProvider>
-</React.StrictMode>
+    <MantineProvider>
+      <RouterProvider router={router} />
+    </MantineProvider>
+  </React.StrictMode>
 );
