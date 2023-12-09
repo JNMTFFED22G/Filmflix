db175c3a3190360bb21a1e18e951667aeecb8cb9
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-12-09 21:19:30 +0100

    added dynamic movie search

--- src/components/SearchModal/SearchModal.tsx
+++ src/components/SearchModal/SearchModal.tsx
@@ -1,23 +1,69 @@
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
+import moviesJSON from '../../data/movies.json';
+import SearchResult from '../SearchResult/SearchResult';
 import './search-modal.css';
 
 interface SearchModalProps {
-  onClose: React.MouseEventHandler<HTMLDivElement>;
+  onClose: (event?: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  onCloseBtnClick: () => void;
 }
 
-export default function SearchModal({ onClose }: SearchModalProps) {
+export default function SearchModal({ onClose, onCloseBtnClick }: SearchModalProps) {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const filteredMovies = moviesJSON.filter(movie => {
+    const lowerCaseQuery = searchQuery.toLowerCase();
+
+    return (
+      movie.title.toLowerCase().includes(lowerCaseQuery) ||
+      movie.year.toString().includes(searchQuery) ||
+      movie.genre.toLowerCase().includes(lowerCaseQuery) ||
+      movie.actors.some(actor => actor.toLowerCase().includes(lowerCaseQuery))
+    );
+  });
+
   return (
+    <>
     <div className='search-modal-overlay' onClick={onClose}>
       <div className='search-modal' onClick={e => e.stopPropagation()}>
-        <div className='search-input-container'>
-          <FontAwesomeIcon icon={faSearch} className='search-icon' />
-          <input
-            type='text'
-            placeholder='Search for title, actor, year or genre'
-          />
+        <div className='input-btn-wrapper'>
+          <div className='search-container'>
+            <FontAwesomeIcon icon={faSearch} className='search-icon' />
+            <input
+              type='text'
+              placeholder='Search for title, actor, year or genre'
+              value={searchQuery}
+              onChange={handleInputChange}
+            />
+          </div>
+          <button className='close-button' onClick={onCloseBtnClick}>
+            <FontAwesomeIcon icon={faTimes} />
+          </button>
+        </div>
+
+       <div className='search-result-container'>
+          <div className='search-results'>
+            {filteredMovies.length === 0 ? (
+              <div className='error-message'>Sorry, no results found.</div>
+            ) : (
+              filteredMovies.map((movie) => (
+                <SearchResult
+                  key={movie.slug}
+                  movie={movie}
+                  onClose={onClose}
+                />
+              ))
+            )}
+          </div>
         </div>
       </div>
     </div>
+    </>
   );
 }
