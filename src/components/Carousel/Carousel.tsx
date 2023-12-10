e56b6ccb27570f07259798c1ee5bdb357ac0ebee
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2023-12-10 22:00:03 +0100

    Dix so that !trend shows rec

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,42 +1,50 @@
+import {
+  faArrowCircleLeft,
+  faArrowCircleRight,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Carousel } from '@mantine/carousel';
-// import { 'fa-arrow-circle-right' as arrowright } from  '@fortawesome/free-regular-svg-icons';
 import iMovie from '../../types/iMovie';
 import Thumbnail from '../Thumbnail/Thumbnail';
-import classes from  './Carousel.module.css';
+import classes from './Carousel.module.css';
 
 interface MovieCarouselProps {
   movie: iMovie[];
   mode: 'rec' | 'trend';
 }
 
-
-
 // Todo
 // More tests need to be written once implemented in the app
 // Add some styling to the carousel, maybe some hover effects?
-// Fix Icons for controllers
 
 function MovieCarousel({ movie, mode }: MovieCarouselProps) {
-  const moviesToDisplay = mode === 'trend' ? movie.filter(movie => movie.isTrending) : movie;
-  // if modes is rec display "recomended movies" else Trending movies in the H1
+  const moviesToDisplay =
+    mode === 'trend' ? movie.filter(movie => movie.isTrending) : movie.filter(movie => !movie.isTrending);
 
-
-
-
-  console.log(movie)
+  console.log(movie);
   return (
     <div className={classes.carouselContainer}>
       <h1>{mode === 'rec' ? 'Recommended Movies' : 'Trending Movies'}</h1>
-      <Carousel
+      <Carousel classNames={classes}
         slideSize={{ base: '60%', sm: '30%', md: '20%' }}
         align='start'
         slideGap='sm'
-        controlsOffset='sm'
-        slidesToScroll={2}
+        controlsOffset=''
+        slidesToScroll={3}
         loop
-        // nextControlIcon={<filledHeart style={{ width: rem(16), height: rem(16) }} />}
-        // previousControlIcon={<FaChevronLeft style={{ width: rem(24), height: rem(24) }} />}
-        >
+        nextControlIcon={
+          <FontAwesomeIcon
+            icon={faArrowCircleRight}
+            className={classes.arrowIcon}
+          />
+        }
+        previousControlIcon={
+          <FontAwesomeIcon
+            icon={faArrowCircleLeft}
+            className={classes.arrowIcon}
+          />
+        }
+      >
         {moviesToDisplay.map((movie, index) => (
           <Carousel.Slide key={index}>
             <Thumbnail movie={movie} mode={mode} />
