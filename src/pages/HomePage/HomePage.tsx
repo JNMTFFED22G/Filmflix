401fdfd12749db592d5ac0f6779c46556d6430e5
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2023-12-10 22:02:05 +0100

    we alla know what homepage is....removed H1 tag

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,7 +4,6 @@ import movies from '../../data/movies.json';
 export default function HomePage() {
   return (
     <div className='nav-padding'>
-      <h1>HomePage</h1>
       <MovieCaruosel movie={movies} mode='trend' />
       <MovieCaruosel movie={movies} mode='rec' />
     </div>
