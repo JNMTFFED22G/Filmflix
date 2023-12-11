47398af27254eea27f264a9d4e46aa05eb051ad4
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2023-12-11 17:51:08 +0100

    Test toggling in film view

--- src/tests/BookmarkButton.test.tsx
+++ src/tests/BookmarkButton.test.tsx
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import Thumbnail from '../components/Thumbnail/Thumbnail';
 import movies from '../data/movies.json';
@@ -15,7 +16,7 @@ describe('BookmarkButton', () => {
   });
 
   it('should render in FilmView', () => {
-    // render(<FilmView />, { wrapper: MemoryRouter });
+    // Render film view with film slug as parameter
     render(
       <MemoryRouter initialEntries={[`/${movie.slug}`]}>
         <Routes>
@@ -25,5 +26,43 @@ describe('BookmarkButton', () => {
     );
 
     expect(screen.getByTestId('bookmark-button')).toBeInTheDocument();
+  });
+
+  it('should be possible to add films to then remove them from the bookmarks', async () => {
+    // Populate with fake ids
+    // This in order to test that only the correct id is added/removed
+    sessionStorage.setItem(
+      'bookmarks',
+      JSON.stringify([2, 3, 4].filter(id => id !== movie.id))
+    );
+
+    const user = userEvent.setup();
+
+    render(
+      <MemoryRouter initialEntries={[`/${movie.slug}`]}>
+        <Routes>
+          <Route path='/:slug' element={<FilmView />} />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    // Double check that the movie is not in the bookmarks
+    expect(
+      JSON.parse(sessionStorage.getItem('bookmarks') || '[]')
+    ).not.toContain(movie.id);
+
+    await user.click(await screen.findByTestId('bookmark-button'));
+
+    // Check that the movie is now in the bookmarks
+    expect(JSON.parse(sessionStorage.getItem('bookmarks') || '[]')).toContain(
+      movie.id
+    );
+
+    await user.click(await screen.findByTestId('bookmark-button'));
+
+    // Check that the movie is now removed from the bookmarks
+    expect(
+      JSON.parse(sessionStorage.getItem('bookmarks') || '[]')
+    ).not.toContain(movie.id);
   });
 });
