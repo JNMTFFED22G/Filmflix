0db18be93deed7c56bb38ecee268af661c83966f
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2023-12-11 18:02:18 +0100

    Test bookmark toggling via thumbnail

--- src/tests/BookmarkButton.test.tsx
+++ src/tests/BookmarkButton.test.tsx
@@ -7,15 +7,15 @@ import movies from '../data/movies.json';
 import FilmView from '../pages/FilmView/FilmView';
 
 describe('BookmarkButton', () => {
-  const movie = movies[0];
-
   it('should render in Thumbnails with mode "rec"', async () => {
+    const movie = movies[0];
     render(<Thumbnail movie={movie} mode='rec' />, { wrapper: MemoryRouter });
 
     expect(await screen.findByTestId('bookmark-button')).toBeInTheDocument();
   });
 
   it('should render in FilmView', () => {
+    const movie = movies[0];
     // Render film view with film slug as parameter
     render(
       <MemoryRouter initialEntries={[`/${movie.slug}`]}>
@@ -28,13 +28,14 @@ describe('BookmarkButton', () => {
     expect(screen.getByTestId('bookmark-button')).toBeInTheDocument();
   });
 
-  it('should be possible to add films to then remove them from the bookmarks', async () => {
+  it('should be possible to toggle bookmarks in film view', async () => {
     // Populate with fake ids
     // This in order to test that only the correct id is added/removed
-    sessionStorage.setItem(
-      'bookmarks',
-      JSON.stringify([2, 3, 4].filter(id => id !== movie.id))
-    );
+    const mockIds = [1, 2, 3, 4, 5];
+
+    const movie = movies.find(m => !mockIds.includes(m.id))!;
+
+    sessionStorage.setItem('bookmarks', JSON.stringify(mockIds));
 
     const user = userEvent.setup();
 
@@ -45,6 +46,39 @@ describe('BookmarkButton', () => {
         </Routes>
       </MemoryRouter>
     );
+
+    // Double check that the movie is not in the bookmarks
+    expect(
+      JSON.parse(sessionStorage.getItem('bookmarks') || '[]')
+    ).not.toContain(movie.id);
+
+    await user.click(await screen.findByTestId('bookmark-button'));
+
+    // Check that the movie is now in the bookmarks
+    expect(JSON.parse(sessionStorage.getItem('bookmarks') || '[]')).toContain(
+      movie.id
+    );
+
+    await user.click(await screen.findByTestId('bookmark-button'));
+
+    // Check that the movie is now removed from the bookmarks
+    expect(
+      JSON.parse(sessionStorage.getItem('bookmarks') || '[]')
+    ).not.toContain(movie.id);
+  });
+
+  it('should be possible to toggle bookmarks in thumbnails', async () => {
+    // Populate with fake ids
+    // This in order to test that only the correct id is added/removed
+    const mockIds = [1, 2, 3, 4, 5];
+
+    const movie = movies.find(m => !mockIds.includes(m.id))!;
+
+    sessionStorage.setItem('bookmarks', JSON.stringify(mockIds));
+
+    const user = userEvent.setup();
+
+    render(<Thumbnail movie={movie} mode='rec' />, { wrapper: MemoryRouter });
 
     // Double check that the movie is not in the bookmarks
     expect(
