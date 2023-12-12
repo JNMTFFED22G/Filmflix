9d62557b857e38d243803b90e1498bd62cc27761
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2023-12-12 11:50:05 +0100

    Added H1 tag "HomePage" back for the test.

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,6 +4,7 @@ import movies from '../../data/movies.json';
 export default function HomePage() {
   return (
     <div className='nav-padding'>
+      <h1>HomePage</h1>
       <MovieCaruosel movie={movies} mode='trend' />
       <MovieCaruosel movie={movies} mode='rec' />
     </div>
