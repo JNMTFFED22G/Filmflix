164872f14251fb123b8b0c23db0dd1e3f27c4eb6
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2023-12-12 10:06:34 +0100

    Add mockimplementation so tests for the MovieCarusel works

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,5 +1,20 @@
 import { cleanup } from '@testing-library/react';
+import "@testing-library/jest-dom/vitest";
+import { afterEach, vi } from "vitest";
 
+// after each run it clears the dom
 afterEach(() => {
   cleanup();
+});
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: vi.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
 });
