37d8e1dfa8a71e3d588ff6470b32cf1d7f1e2311
Jenny Pettersson <j.m.pettersson@hotmail.se>
2023-12-14 11:00:15 +0100

    hero styled

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,7 +1,20 @@
+import classes from './HeroSection.module.css';
+
 function HeroSection() {
   return (
-    <div>
-      <h1>HeroSection</h1>
+    <div className={classes.outerContainer}>
+      <div className={classes.textContainer}>
+        <div className={classes.titleRow}>
+          <h1 className={classes.title}>Whiplash</h1>
+          <p>2014</p>
+        </div>
+        <p className={classes.infoText}>
+          A promising young drummer enrolls at a cut-throat music conservatory
+          where his dreams of greatness are mentored by an instructor who will
+          stop at nothing to realize a student's potential.
+        </p>
+        <button className={classes.button}>Watch now</button>
+      </div>
     </div>
   );
 }
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,12 +4,14 @@ import movies from '../../data/movies.json';
 
 export default function HomePage() {
   return (
-    <div className='nav-padding'>
+    <>
       <HeroSection />
-      <h1>HomePage</h1>
-      {movies.map((movie, index) => (
-        <Thumbnail movie={movie} mode='rec' key={index} />
-      ))}
-    </div>
+      <div className='nav-padding'>
+        <h1>HomePage</h1>
+        {movies.map((movie, index) => (
+          <Thumbnail movie={movie} mode='rec' key={index} />
+        ))}
+      </div>
+    </>
   );
 }
