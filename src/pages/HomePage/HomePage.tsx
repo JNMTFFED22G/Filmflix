d9f5ca45af1c84ba8f3481eee9118f1dacfcded3
Jenny Pettersson <j.m.pettersson@hotmail.se>
2023-12-14 14:13:45 +0100

    padding bottom

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,13 @@
 import MovieCaruosel from '../../components/Carousel/Carousel';
 import HeroSection from '../../components/HeroSection/HeroSection';
 import movies from '../../data/movies.json';
+import classes from './HomePage.module.css';
 
 export default function HomePage() {
   return (
     <>
       <HeroSection />
-      <div>
+      <div className={classes.mainContainer}>
         <MovieCaruosel movie={movies} mode='trend' />
         <MovieCaruosel movie={movies} mode='rec' />
       </div>
