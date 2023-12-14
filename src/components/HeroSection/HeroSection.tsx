7c5edb6f7401a58a38a8cfb5aa7440633b8c01ad
Jenny Pettersson <j.m.pettersson@hotmail.se>
2023-12-14 10:36:39 +0100

    create function

---
+++ src/components/HeroSection/HeroSection.tsx
@@ -0,0 +1,9 @@
+function HeroSection() {
+  return (
+    <div>
+      <h1>HeroSection</h1>
+    </div>
+  );
+}
+
+export default HeroSection;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,11 @@
+import HeroSection from '../../components/HeroSection/HeroSection';
 import Thumbnail from '../../components/Thumbnail/Thumbnail';
 import movies from '../../data/movies.json';
 
 export default function HomePage() {
   return (
     <div className='nav-padding'>
+      <HeroSection />
       <h1>HomePage</h1>
       {movies.map((movie, index) => (
         <Thumbnail movie={movie} mode='rec' key={index} />
